f853240da99e4a93373da7de3bde5a7d62fc986a
Umut Acerbastimur <33218120+acerbastimur@users.noreply.github.com>
2018-06-13 12:42:51 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ fonksiyon() {
     // if the target of the click isn't the container nor a descendant of the container
     if (!container.is(e.target) && container.has(e.target).length === 0) 
     {
-        container.hide();
+        container.hide(300);
     }
 });
 }}
