a16dc7ead0c858ab5477fdf2d15ba5b4e8e6c08a
acerbastimur <umutacerbastimur@gmail.com>
2018-06-17 17:14:31 +0300

    a bit bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,7 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  news;
-  images = ['http://i.hizliresim.com/vpP0kO.jpg','http://i.hizliresim.com/M0gZVk.jpg','http://i.hizliresim.com/vpP0kO.jpg']
-    
-  
+  news;  
   constructor(afDB: AngularFireDatabase,config: NgbCarouselConfig) { 
     this.news = afDB.list('news').valueChanges();
     config.interval = 4000;
