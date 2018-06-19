270c80e487bac80695ecfc214e0c6c51f90724c9
Hasan <hasanharman33@gmail.com>
2018-06-20 00:08:09 +0300

    homedenendiolmadı

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,16 +8,23 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  news;  
-  constructor(afDB: AngularFireDatabase,config: NgbCarouselConfig) { 
-    this.news = afDB.list('news').valueChanges();
+  news;
+  facultynews;
+  announcements;
+  events;
+  graduates;
+  constructor(afDB: AngularFireDatabase,config: NgbCarouselConfig,db: AngularFireDatabase) { 
+    this.news = afDB.list('home/news').valueChanges();
     config.interval = 4000;
     config.wrap = true;
     config.keyboard = true;
-
+    this.facultynews = afDB.list('home/facultynews').valueChanges();
+    this.announcements = afDB.list('home/announcements').valueChanges();
+    this.events = db.database.ref("home/events");
+    // afDB.list('home/events').valueChanges();
+    this.graduates = afDB.list('home/graduates').valueChanges();
+    
   }
-
   ngOnInit() {
   }
-
 }
