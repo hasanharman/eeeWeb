286d7d30b1be138caa82a42fc1499fd189c67cb8
acerbastimur <umutacerbastimur@gmail.com>
2018-07-17 09:07:57 +0300

    news details page was editted

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as firebase from 'firebase/app';
+import { TranslateService, LangChangeEvent } from '../../../node_modules/ng2-translate';
 
 @Component({
   selector: 'app-news',
@@ -14,16 +15,38 @@ export class NewsComponent implements OnInit {
   text:string;
   photoUrl:string;
   subtitle:string;
-  constructor(private route: ActivatedRoute ) {
+  constructor(private route: ActivatedRoute,translateService:TranslateService ) {
+    translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event.lang);
+      this.starterLang = event.lang;
+      
+      firebase.database().ref().child(this.starterLang).child('home').child('news').child(this.routerDirective)
+      .once('value',news => {
+        console.log(news.val());
+        
+        this.title = news.child('newsDetail').child('title').val(); 
+        this.text = news.child('newsDetail').child('text').val();
+        this.photoUrl = news.child('newsDetail').child('photoUrl').val(); 
+        this.subtitle = news.child('newsDetail').child('subtitle').val(); 
+        console.log(this.title);
+        
+      })
+
+    });
+
     this.route.params.subscribe(params => {
       this.routerDirective = params['newsId'];
-
-      firebase.database().ref().child('tr').child('home').child('news').child(this.routerDirective)
+      console.log(this.routerDirective);
+      
+      firebase.database().ref().child(this.starterLang).child('home').child('news').child(this.routerDirective)
       .once('value',news => {
-        this.title = news.child('newsDetails').child('title').val(); 
-        this.text = news.child('newsDetails').child('text').val();
-        this.photoUrl = news.child('newsDetails').child('photoUrl').val(); 
-        this.subtitle = news.child('newsDetails').child('subtitle').val(); 
+        console.log(news.val());
+        
+        this.title = news.child('newsDetail').child('title').val(); 
+        this.text = news.child('newsDetail').child('text').val();
+        this.photoUrl = news.child('newsDetail').child('photoUrl').val(); 
+        this.subtitle = news.child('newsDetail').child('subtitle').val(); 
+        console.log(this.title);
         
       })
     });
