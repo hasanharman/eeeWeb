ba71db7de0fe8fe1fca098401adefd7da83c5277
Hasan <hasanharman33@gmail.com>
2018-07-28 17:40:40 +0300

    news1 page changed, about changed

--- src/app/about/abouteee/abouteee.component.ts
+++ src/app/about/abouteee/abouteee.component.ts
@@ -12,9 +12,19 @@ import { TranslateService, LangChangeEvent } from '../../../../node_modules/ng2-
 export class AbouteeeComponent implements OnInit {
   title;
   text;
+  ppAylin;
+  ppMuh;
   starterLang = 'tr'
 
-  constructor(afDB: AngularFireDatabase, translateService: TranslateService) { 
+  constructor(afDB: AngularFireDatabase, translateService: TranslateService) {
+
+    firebase.database().ref().child(this.starterLang).child('personels').child('spotlights').once('value', spotlights=> {
+      this.ppAylin = spotlights.child('mezun2').child('pp').val();
+    })
+
+    firebase.database().ref().child(this.starterLang).child('personels').child('spotlights').once('value', spotlights=> {
+      this.ppMuh = spotlights.child('mezun3').child('pp').val();
+    })
      
   }
 
