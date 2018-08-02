2579e07f07970fea5d94866d0331e10929b888cf
acerbastimur <umutacerbastimur@gmail.com>
2018-08-02 14:08:56 +0300

    hasan

--- src/app/education/graduate/graduate.component.ts
+++ src/app/education/graduate/graduate.component.ts
@@ -1,3 +1,5 @@
+import { TranslateService, LangChangeEvent } from 'ng2-translate';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graduate.component.css']
 })
 export class GraduateComponent implements OnInit {
+  starterLang = 'tr';
+  collection;
+  constructor(private afDB: AngularFireDatabase, translateService: TranslateService) { 
+    if(!translateService.currentLang)  this.starterLang = 'tr' ;
+    else this.starterLang = translateService.currentLang;
+    translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event.lang);
+      this.starterLang = event.lang;
+      this.collection = afDB.list(this.starterLang + '/education/graduate').valueChanges();
+    });
+    this.collection = afDB.list(this.starterLang + '/education/graduate').valueChanges();
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
-
 }
