e12e49451f238d8fcc6bddf88f7df27d73570abf
Hasan <hasanharman33@gmail.com>
2018-08-02 13:36:52 +0300

    new

--- src/app/announcements/announcements.component.ts
+++ src/app/announcements/announcements.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { ActivatedRoute } from '@angular/router';
-import * as firebase from 'firebase/app';
 import { TranslateService, LangChangeEvent } from '../../../node_modules/ng2-translate';
+
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
--- src/app/education/undergraduate/undergraduate.component.ts
+++ src/app/education/undergraduate/undergraduate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { TranslateService, LangChangeEvent } from '../../../../node_modules/ng2-translate';
 
 @Component({
   selector: 'app-undergraduate',
@@ -6,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./undergraduate.component.css']
 })
 export class UndergraduateComponent implements OnInit {
+  starterLang = 'tr';
 
-  constructor() { }
+  constructor(private afDB: AngularFireDatabase, translateService: TranslateService) { 
+    if(!translateService.currentLang)  this.starterLang = 'tr' ;
+    else this.starterLang = translateService.currentLang;
+    translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event.lang);
+      this.starterLang = event.lang;
+
+    });
+
+  }
 
   ngOnInit() {
   }
