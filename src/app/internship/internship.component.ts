b91e7a79676521fccb1752b565f65fd346bff6de
Hasan <hasanharman33@gmail.com>
2018-08-05 21:49:34 +0300

    project-internpage added

--- src/app/internship/internship.component.ts
+++ src/app/internship/internship.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { TranslateService, LangChangeEvent } from '../../../node_modules/ng2-translate';
 
 @Component({
   selector: 'app-internship',
@@ -6,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./internship.component.css']
 })
 export class InternshipComponent implements OnInit {
+  starterLang = 'tr';
+  courseList = false;
+  collection;
+  
+  constructor(private afDB: AngularFireDatabase, translateService: TranslateService) { 
+    if(!translateService.currentLang)  this.starterLang = 'tr' ;
+    else this.starterLang = translateService.currentLang;
+    translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event.lang);
+      this.starterLang = event.lang;
+      this.collection = afDB.list(this.starterLang + '/internship').valueChanges();
+    });
+    this.collection = afDB.list(this.starterLang + '/internship').valueChanges();
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
--- src/app/project-lessons/project-lessons.component.ts
+++ src/app/project-lessons/project-lessons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { TranslateService, LangChangeEvent } from '../../../node_modules/ng2-translate';
 
 @Component({
   selector: 'app-project-lessons',
@@ -6,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-lessons.component.css']
 })
 export class ProjectLessonsComponent implements OnInit {
+  starterLang = 'tr';
+  courseList = false;
+  collection;
+  
+  constructor(private afDB: AngularFireDatabase, translateService: TranslateService) { 
+    if(!translateService.currentLang)  this.starterLang = 'tr' ;
+    else this.starterLang = translateService.currentLang;
+    translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event.lang);
+      this.starterLang = event.lang;
+      this.collection = afDB.list(this.starterLang + '/projectLesson').valueChanges();
+    });
+    this.collection = afDB.list(this.starterLang + '/projectLesson').valueChanges();
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
