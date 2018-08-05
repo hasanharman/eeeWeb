9e6627aec10c8964054a517bcd4c00e1662ab153
Hasan <hasanharman33@gmail.com>
2018-08-05 22:14:59 +0300

    contact en added

--- src/app/about/contact/contact.component.ts
+++ src/app/about/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { TranslateService, LangChangeEvent } from '../../../../node_modules/ng2-translate';
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  starterLang = 'tr';
+  courseList = false;
+  collection;
+  
+  constructor(private afDB: AngularFireDatabase, translateService: TranslateService) { 
+    if(!translateService.currentLang)  this.starterLang = 'tr' ;
+    else this.starterLang = translateService.currentLang;
+    translateService.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event.lang);
+      this.starterLang = event.lang;
+      this.collection = afDB.list(this.starterLang + 'navbar/contactDetails').valueChanges();
+    });
+    this.collection = afDB.list(this.starterLang + 'navbar/contactDetails').valueChanges();
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
