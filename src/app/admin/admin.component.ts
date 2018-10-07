b478ed84b40a55460dd4a1a8cd2dc19661efe899
acerbastimur <umutacerbastimur@gmail.com>
2018-10-07 16:41:38 +0300

    admin-announcements

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase/app';
+declare var jdetects: any;
 
 @Component({
   selector: 'app-admin',
@@ -8,20 +9,69 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-pass:string;
-logged:boolean = true;
-  constructor() { 
-firebase.database().ref().once('value',data => {
-this.pass=data.child('adminPass').val();
-})
+  panel = false;
+  entered = true;
+  userName: string;
+  constructor() {
+
   }
 
   ngOnInit() {
+
+
+
+
   }
- login(password) {
-   console.log(password);
-   if(this.pass === password)
-   alert('succesfull');
-   this.logged = true;
- }
+
+  login() {
+
+    firebase.database().ref('/admins').once('value', val => {
+      val.forEach(element => {
+        if (this.entered) {
+          if ($('.mail').val() == element.child('mail').val()) {
+            if ($('.password').val() == element.child('password').val()) {
+              console.log('You have succesfuly entered');
+              this.entered = false;
+              this.panel = true;
+              this.userName = element.child('name').val();
+
+            } else {
+              console.log('Wrong Password');
+              $(document).remove()
+              location.reload();
+            }
+          }
+        }
+
+      });
+    });
+  }
+
+  announcement() {
+    /* EN */
+    let key;
+    firebase.database().ref('/en/announcements').push({
+      title: $('#notification-title-en').val(),
+      text: $('#notification-text-en').val(),
+      pp: $('#notification-photo-en').val(),
+      type: parseInt($('#notification-type').val().toString())
+    }).then((snap) => {
+      console.log(snap);
+      firebase.database().ref('/en/announcements/' + snap.key).update({
+        key: snap.key
+      })
+      key = snap.key;
+      /* TR */
+
+      firebase.database().ref('/tr/announcements/' + key).update({
+        title: $('#notification-title-tr').val(),
+        text: $('#notification-text-tr').val(),
+        pp: $('#notification-photo-tr').val(),
+        type: parseInt($('#notification-type').val().toString()),
+        key: key
+      }) 
+    })
+
+
+  }
 }
