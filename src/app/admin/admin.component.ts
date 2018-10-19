3b4afd948788343f7ce268d93dfa108b8459f763
acerbastimur <umutacerbastimur@gmail.com>
2018-10-19 12:08:54 +0300

    started to do events

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -158,15 +158,81 @@ export class AdminComponent implements OnInit {
     const place_en = $('#news-events-en').val();
     const place_tr = $('#news-events-tr').val();
     const photo = $('#news-photo').val();
+    const speaker = $('#news-speaker').val();
     const text1_en = $('#news-text1-en').val();
     const text1_tr = $('#news-text1-tr').val();
     const text2_en = $('#news-text2-en').val();
     const text2_tr = $('#news-text2-tr').val();
     const text3_en = $('#news-text3-en').val();
     const text3_tr = $('#news-text3-tr').val();
-    console.log(place_en,place_tr,photo,text1_en,text1_tr,text2_en,text2_tr,text3_en,text3_tr)
+    const title_en = $('#news-title-en').val();
+    const title_tr = $('#news-title-tr').val();
+    const topic_en = $('#news-topic-en').val();
+    const topic_tr = $('#news-topic-tr').val();
+    const date = $('#news-date').val().toString();
 
-    
-
+      function getTimeStamp(input) {
+      var parts = input.trim().split(' ');
+      var date = parts[0].split('-');
+    var time = (parts[1] ? parts[1] : '00:00:00').split(':');
+  
+    // NOTE:: Month: 0 = January - 11 = December.
+    var d = new Date(date[0],date[1]-1,date[2],time[0],time[1],time[2]);
+    return d.getTime() / 1000;
   }
+
+  function parseDate(date:string) {
+    var newDate = date.split('-');
+    var mS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+    newDate[1] =mS[parseInt(newDate[1]) - 1]
+     return newDate;
+  }
+  
+  console.log(parseDate(date))
+
+
+    /*   HERE COMES THE EN PART*/
+      
+    firebase.database().ref('/en/events').push({
+           place:place_en,
+           pp:photo,
+           year:parseDate(date)[0],
+           month:parseDate(date[1]),
+           day:parseDate(date[2]),
+           title:title_en,
+           eventDetail: {
+             pp:photo,
+             speaker:speaker,
+             text1: text1_en,
+             text2:text2_en,
+             text3:text3_en,
+             title:title_en,
+             topic:topic_en,
+             venue:place_en
+           }
+
+    }).then(data => {
+      firebase.database().ref('/en/events/' + data.key).push({
+      eventName: data.key
+      });
+    })
+ 
+
+      /*   HERE COMES THE TR PART*/
+     /* 
+      firebase.database().ref('/tr/events').push({
+        place:place_en,
+        pp:photo,
+        title:title_en
+
+ }).then(data => {
+   firebase.database().ref('/tr/events/' + data.key).push({
+   eventName: data.key
+   });
+ })
+  } */
+
 }
+
+
